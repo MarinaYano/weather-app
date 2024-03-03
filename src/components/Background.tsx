fcbd95c6c0879d89b9079fbe75bf97321ce19acf
MarinaYano <myano2621@gmail.com>
2024-03-03 03:59:06 -0800

    feat: set up background image

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,33 +1,6 @@
-// import { useEffect, useState } from "react"
-
 const Background = () => {
-  // const [video, setVideo] = useState<string>('')
-  // useEffect(() => {
-  //   fetchVideo('sunny')
-  // }, [])
-
-  // const fetchVideo = async (query: string) => {
-  //   const videoApiKey = "ojuQefv1lHLel7nJWsAyAFD8j51ivMi2Bpl0NFl1GJPLc9SdpwYn3NFc"
-  //   const res = await fetch(`https://api.pexels.com/videos/search?query=${query}`, {
-  //     headers: {
-  //       Authorization: `${videoApiKey}`,
-  //     },
-  //     credentials: "same-origin",
-  //   })
-
-  //   if (!res.ok) {
-  //     throw new Error(`Failed to fetch video: ${res.status} - ${res.statusText}`);
-  //   }
-
-  //   const data = await res.json()
-  //   setVideo(data.videos[0].url)
-  // }
   return (
-    // <video 
-    //   autoPlay muted loop src={video} 
-    //   className="w-full h-full object-cover fixed top-0 left-0 z-01"
-    // />
-    <div className="bg-[url('/bg.png')] h-full w-full bg-cover absolute top-0 left-0 z-99">
+    <div className="bg-[url('https://source.unsplash.com/1600x900/?tokyo&sunny')] h-full w-full bg-cover absolute top-0 left-0 z-99">
     </div>
   )
 }
