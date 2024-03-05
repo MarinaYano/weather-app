43fad66ed6ae7cf8b86f661ab776f4bbaa57a8cf
MarinaYano <myano2621@gmail.com>
2024-03-04 16:54:54 -0800

    feat: show Hourly Forecast section

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ import axios from "axios";
 import CityInfo from './components/CityInfo'
 
 export type Weather = {
-  temp: number;
+  temp_c: number;
+  temp_f: number;
   condition: string;
   high: number;
   low: number;
@@ -20,17 +21,27 @@ export type Weather = {
   localTime: string;
 }
 
+export type Hour = {
+  chanceOfRain: number;
+  icon: string;
+  temp_c: number;
+  temp_f: number;
+  time: string;
+};
+
 function App() {
   const [weather, setWeather] = useState<Weather>()
+  const [hourData, setHourData] = useState<Hour[]>([])
+  const [loading, setLoading] = useState<boolean>(true);
 
   const fetchWeatherInfo = (input: string) => {
     const WEATHER_API_KEY = import.meta.env.VITE_APP_WEATHER_API_KEY
   
     axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${input}&days=3`)
       .then(res => {
-        console.log(res.data)
         setWeather({
-          temp: res.data.current.temp_c,
+          temp_c: res.data.current.temp_c,
+          temp_f: res.data.current.temp_f,
           condition: res.data.current.condition.text,
           high: res.data.forecast.forecastday[0].day.maxtemp_c,
           low: res.data.forecast.forecastday[0].day.mintemp_c,
@@ -39,11 +50,22 @@ function App() {
           uv: res.data.current.uv,
           moon: res.data.forecast.forecastday[0].astro.moon_phase,
           cityName: res.data.location.name,
-          localTime: res.data.location.localtime
+          localTime: res.data.location.localtime,
         })
+
+        const hourlyForecasts = res.data.forecast.forecastday[0].hour;
+        setHourData(hourlyForecasts.map((hour: any) => ({
+          chanceOfRain: hour.chance_of_rain,
+          icon: hour.condition.icon,
+          temp_c: hour.temp_c,
+          temp_f: hour.temp_f,
+          time: hour.time,
+        })));
+        setLoading(false)
       })
       .catch((error) => {
         console.log("ERROR: ", error.message)
+        setLoading(false)
       })
   }
 
@@ -53,11 +75,16 @@ function App() {
 
   return (
     <div className='w-10/12 sm:max-w-screen-lg my-0 mx-auto h-screen overflow-scroll'>
-      <Background weather={weather} />
-      <Search onSearch={fetchWeatherInfo} />
-      <CityInfo weather={weather} />
-      <Detail weather={weather} />
-      <HourlyForecast />
+      {loading ? (
+        <p>Loading</p>
+      ) : (<>
+        <Background weather={weather} />
+        <Search onSearch={fetchWeatherInfo} />
+        <CityInfo weather={weather} />
+        <Detail weather={weather} />
+        <HourlyForecast hourData={hourData} />
+      </>)}
+      
     </div>
   )
 }
