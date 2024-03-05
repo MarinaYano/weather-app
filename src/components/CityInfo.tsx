6cebb34d3383855ac242f2ac1c355fa2ec7aa007
MarinaYano <myano2621@gmail.com>
2024-03-04 18:56:37 -0800

    chore: modify the format of date and time

--- src/components/CityInfo.tsx
+++ src/components/CityInfo.tsx
@@ -1,14 +1,16 @@
 import { Weather } from '../App'
 
 interface CityInfoProp {
-  weather: Weather | undefined;
+  weather: Weather;
 }
 
 const CityInfo: React.FC<CityInfoProp> = ({ weather }) => {
+  const formattedTime = new Date(weather.localTime).toLocaleDateString([], { month: 'long', day: 'numeric', hour: 'numeric', minute: '2-digit', hour12: true })
+
   return (
     <div className="w-full text-center my-5 relative z-1">
       <h1 className="text-xl text-white sm:text-4xl sm:mb-2">{weather?.cityName}</h1>
-      <p className="text-sm text-white sm:text-base">{weather?.localTime}</p>
+      <p className="text-sm text-white sm:text-base">{formattedTime}</p>
     </div>
   )
 }
